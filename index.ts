3a4b55b3c6f93ad9b73c145ce12ae2eec30a5a20
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-03-24 20:42:08 -0400

    feat: create first input from terminal

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log('Hello World')
+import readline from "readline";
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+terminal.question("Enter some comma-separated numbers to generate a combination: ", (numbers: string) => {
+  console.log('numbers ', numbers);
+  
+  terminal.close();
+});
+
+terminal.on("close", () => {
+  console.log("\nBYE BYE !!!");
+  process.exit(0);
+});
