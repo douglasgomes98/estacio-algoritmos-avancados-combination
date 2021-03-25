c495aa5d4723fab7ad7850db74221ad495bb0e35
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-03-24 21:15:43 -0400

    feat: create mountNumbers function

--- index.ts
+++ index.ts
@@ -6,13 +6,32 @@ const terminal = readline.createInterface({
   terminal: false,
 });
 
-terminal.question("Enter some comma-separated numbers to generate a combination: ", (numbers: string) => {
-  console.log('numbers ', numbers);
-  
-  terminal.close();
-});
+function mountNumbers(input: string) {
+  const numbers: number[] = [];
+
+  const inputValues = input.split(",");
+
+  inputValues.map(
+    (value) =>
+      (value === "1" || value === "0") &&
+      value.length === 1 &&
+      numbers.push(Number(value))
+  );
+
+  return numbers;
+}
+
+terminal.question(
+  "Enter some comma-separated numbers to generate a combination: ",
+  (numbersInput: string) => {
+    const numbersMap: number[] = mountNumbers(numbersInput);
+
+    console.log(numbersMap);
+
+    terminal.close();
+  }
+);
 
 terminal.on("close", () => {
-  console.log("\nBYE BYE !!!");
   process.exit(0);
 });
